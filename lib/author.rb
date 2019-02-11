35942e77a8f6d03f23358a9d1ff0a04a73583ea9
blackwell <blackwellsmithiv@gmail.com>
2019-02-11 13:55:26 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,6 @@
 require "pry"
 class Author 
-  attr_accessor :name 
+  attr_accessor :name, :post 
   def initialize(name)
     @name = name 
     @posts = []
@@ -8,4 +8,8 @@ class Author
   def posts
     @posts 
   end
+  def add_post(post)
+    self.post
+  end 
+  def add_post_by_title(post)
 end
